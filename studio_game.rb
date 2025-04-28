1b9ff7caa4bb9916a98fcfc5e5d7a526f7d25777
Anna Vasileva <vasilieva.a@icloud.com>
2025-04-27 21:44:56 -0400

    add a player class

--- studio_game.rb
+++ studio_game.rb
@@ -1,9 +1,39 @@
-def say_hello (name:, health:100)
-  "I'm #{name.capitalize} with a health of #{health}"
-end   
+class Player 
+  def initialize(name, health = 100)
+    @name = name.capitalize
+    @health = health
+  end  
+
+  def say_hello
+    "I'm #{@name} with a health of #{@health}"
+  end
+
+  def drain
+    @health -= 10
+  end 
+
+  def boost
+    @health += 15
+  end
+
+end
+
+player_1 = Player.new("finn", 60)
+puts player_1.say_hello
+player_1.drain
+puts player_1.say_hello
+
+player_2 = Player.new("lucy", 90)
+puts player_2.say_hello
+
+player_3 = Player.new("jase")
+puts player_3.say_hello
+
+player_4 = Player.new("alex", 125)
+player_4.drain
+puts player_4.say_hello
+player_4.boost
+puts player_4.say_hello
 
 
-puts say_hello(health: 60, name: "finn")
-puts say_hello(health: 90, name: "lucy")
-puts say_hello(name: "jase")
-puts say_hello(health:125, name: "alex")
+
