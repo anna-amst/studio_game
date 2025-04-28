73f2d3ddde06e4d0327ce587fbde26671a3a8c80
Anna Vasileva <vasilieva.a@icloud.com>
2025-04-28 11:08:20 -0400

    add an array

--- studio_game.rb
+++ studio_game.rb
@@ -26,42 +26,39 @@ class Player
 end
 
 player_1 = Player.new("finn", 60)
+player_2 = Player.new("lucy", 90)
+player_3 = Player.new("jase")
+player_4 = Player.new("alex", 125)
 
+players = [player_1, player_2, player_3, player_4]
+
+players.pop
+player_5 = Player.new("Cole", 75)
+players.push(player_5)
+
+puts "Before playing:"
+puts players
+
+players.each do |player|
 number_rolled = rand(1..6)
 
 case number_rolled
 when 1..2
-  player_1.drain
-  puts "#{player_1.name} got drained 😫"
+  player.drain
+  puts "#{player.name} got drained 😫"
 when 3..4
-  puts "#{player_1.name} got skipped"
+  puts "#{player.name} got skipped"
 else 
-  player_1.boost
-  puts "#{player_1.name} got boosted 😁"
-end  
+  player.boost
+  puts "#{player.name} got boosted 😁"
+end 
+
+end
+
+puts "\nAfter playing:"
+puts players
+
+ 
 
 
-# puts player_1.to_s
-# player_1.drain
-# puts player_1.to_s
 
-# player_2 = Player.new("lucy", 90)
-# puts player_2.to_s
-
-# player_3 = Player.new("jase")
-# puts player_3.to_s
-
-# player_4 = Player.new("alex", 125)
-# player_4.drain
-# puts player_4
-
-# puts player_1.health
-# puts player_2.health
-# puts player_3.health
-# puts player_4.health
-
-# puts player_1.name
-# puts player_2.name
-# puts player_3.name
-# player_4.name = "Alexander"
-# puts player_4.name
