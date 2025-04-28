2467e01e02d47506b6d3dd98f9005bea94e4394c
Anna Vasileva <vasilieva.a@icloud.com>
2025-04-28 17:05:23 -0400

    Add a game class

--- studio_game.rb
+++ studio_game.rb
@@ -25,38 +25,61 @@ class Player
 
 end
 
+class Game
+  attr_reader :title, :players
+
+
+  def initialize(title)
+    @title = title
+    @players = []
+  end
+
+  def add_player(player)
+    @players << player
+    # @players.push(player)
+    # @players.append(player)
+  end
+
+  def roll_die
+    rand(1..6)
+  end
+
+  def play
+    puts "Before playing #{@title}:"
+    puts @players
+
+    @players.each do |player|
+      number_rolled = roll_die
+      
+      case number_rolled
+      when 1..2
+        player.drain
+        puts "#{player.name} got drained 😫"
+      when 3..4
+        puts "#{player.name} got skipped"
+      else 
+        player.boost
+        puts "#{player.name} got boosted 😁"
+      end 
+      end
+    
+    puts "\nAfter playing:"  
+    puts @players
+  end
+end   
+
 player_1 = Player.new("finn", 60)
 player_2 = Player.new("lucy", 90)
 player_3 = Player.new("jase")
 player_4 = Player.new("alex", 125)
 
-players = [player_1, player_2, player_3, player_4]
+game = Game.new("Winner Takes All")
+game.add_player(player_1)
+game.add_player(player_2)
+game.add_player(player_3)
+game.add_player(player_4)
+game.play
 
-players.pop
-player_5 = Player.new("Cole", 75)
-players.push(player_5)
-
-puts "Before playing:"
-puts players
-
-players.each do |player|
-number_rolled = rand(1..6)
-
-case number_rolled
-when 1..2
-  player.drain
-  puts "#{player.name} got drained 😫"
-when 3..4
-  puts "#{player.name} got skipped"
-else 
-  player.boost
-  puts "#{player.name} got boosted 😁"
-end 
-
-end
-
-puts "\nAfter playing:"
-puts players
 
  
 
