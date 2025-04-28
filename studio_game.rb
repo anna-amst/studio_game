5b6db0ff075adf857a3c313783c1874fff01a6f5
Anna Vasileva <vasilieva.a@icloud.com>
2025-04-27 23:42:15 -0400

    add a condition

--- studio_game.rb
+++ studio_game.rb
@@ -26,30 +26,42 @@ class Player
 end
 
 player_1 = Player.new("finn", 60)
-puts player_1.to_s
-player_1.drain
-puts player_1.to_s
 
-player_2 = Player.new("lucy", 90)
-puts player_2.to_s
+number_rolled = rand(1..6)
 
-player_3 = Player.new("jase")
-puts player_3.to_s
-
-player_4 = Player.new("alex", 125)
-player_4.drain
-puts player_4
-
-puts player_1.health
-puts player_2.health
-puts player_3.health
-puts player_4.health
-
-puts player_1.name
-puts player_2.name
-puts player_3.name
-player_4.name = "Alexander"
-puts player_4.name
+case number_rolled
+when 1..2
+  player_1.drain
+  puts "#{player_1.name} got drained 😫"
+when 3..4
+  puts "#{player_1.name} got skipped"
+else 
+  player_1.boost
+  puts "#{player_1.name} got boosted 😁"
+end  
 
 
+# puts player_1.to_s
+# player_1.drain
+# puts player_1.to_s
 
+# player_2 = Player.new("lucy", 90)
+# puts player_2.to_s
+
+# player_3 = Player.new("jase")
+# puts player_3.to_s
+
+# player_4 = Player.new("alex", 125)
+# player_4.drain
+# puts player_4
+
+# puts player_1.health
+# puts player_2.health
+# puts player_3.health
+# puts player_4.health
+
+# puts player_1.name
+# puts player_2.name
+# puts player_3.name
+# player_4.name = "Alexander"
+# puts player_4.name
